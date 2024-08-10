54d9ba16fcbc2f7cadef8909b70bd41b58f8db6f
googer <googer@mindurka.fun>
2024-08-10 05:35:32 +0000

    Apply automatic reformat

--- browser/config.py
+++ browser/config.py
@@ -1,13 +1,17 @@
 BROWSER_NAME = "googer's Browser"
 BROWSER_HOME = "goog://home"
-BROWSER_USERAGENT_MOBILE = ("Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) "
-                            "Chrome/126.0.0.0 Mobile Safari/537.3")
-BROWSER_USERAGENT_PC = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                        "Chrome/126.0.0.0 Safari/537.3")
+BROWSER_USERAGENT_MOBILE = (
+    "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/126.0.0.0 Mobile Safari/537.3"
+)
+BROWSER_USERAGENT_PC = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/126.0.0.0 Safari/537.3"
+)
 
 TAB_TITLE_CUTOFF = 20
 
-PROXY_LISTEN_HOST = '127.0.0.1'
+PROXY_LISTEN_HOST = "127.0.0.1"
 PROXY_LISTEN_PORT = 6913
 PROXY_BUFFER_SIZE = 1024 * 64
 PROXY_ACTIVE = True
--- browser/front/page.py
+++ browser/front/page.py
@@ -13,33 +13,35 @@ class Page(QWidget):
         self.window = window
 
         self.toolbar = {
-            'back': QPushButton('Back'),
-            'forward': QPushButton('Forward'),
-            'refresh': QPushButton('Refresh'),
-            'home': QPushButton('Home'),
-            'useragent': QPushButton('PC'),
-            'urlbar': QLineEdit(),
-            'progressbar': QProgressBar()
+            "back": QPushButton("Back"),
+            "forward": QPushButton("Forward"),
+            "refresh": QPushButton("Refresh"),
+            "home": QPushButton("Home"),
+            "useragent": QPushButton("PC"),
+            "urlbar": QLineEdit(),
+            "progressbar": QProgressBar(),
         }
-        self.toolbar['back'].setIcon(QIcon("assets/icons/back.svg"))
-        self.toolbar['back'].clicked.connect(lambda: self.back())
+        self.toolbar["back"].setIcon(QIcon("assets/icons/back.svg"))
+        self.toolbar["back"].clicked.connect(lambda: self.back())
 
-        self.toolbar['forward'].setIcon(QIcon("assets/icons/forward.svg"))
-        self.toolbar['forward'].clicked.connect(lambda: self.forward())
+        self.toolbar["forward"].setIcon(QIcon("assets/icons/forward.svg"))
+        self.toolbar["forward"].clicked.connect(lambda: self.forward())
 
-        self.toolbar['refresh'].setIcon(QIcon("assets/icons/refresh.svg"))
-        self.toolbar['refresh'].clicked.connect(lambda: self.refresh())
+        self.toolbar["refresh"].setIcon(QIcon("assets/icons/refresh.svg"))
+        self.toolbar["refresh"].clicked.connect(lambda: self.refresh())
 
-        self.toolbar['home'].setIcon(QIcon("assets/icons/home.svg"))
-        self.toolbar['home'].clicked.connect(lambda: self.home())
+        self.toolbar["home"].setIcon(QIcon("assets/icons/home.svg"))
+        self.toolbar["home"].clicked.connect(lambda: self.home())
 
-        self.toolbar['useragent'].setIcon(QIcon("assets/icons/useragent_pc.svg"))
-        self.toolbar['useragent'].clicked.connect(lambda: self.toggleUserAgent())
+        self.toolbar["useragent"].setIcon(QIcon("assets/icons/useragent_pc.svg"))
+        self.toolbar["useragent"].clicked.connect(lambda: self.toggleUserAgent())
 
-        self.toolbar['urlbar'].returnPressed.connect(lambda: self.setURL(self.toolbar['urlbar'].text()))
+        self.toolbar["urlbar"].returnPressed.connect(
+            lambda: self.setURL(self.toolbar["urlbar"].text())
+        )
 
-        self.toolbar['progressbar'].setValue(100)
-        self.toolbar['progressbar'].setTextVisible(False)
+        self.toolbar["progressbar"].setValue(100)
+        self.toolbar["progressbar"].setTextVisible(False)
 
         self.layout = QVBoxLayout(self)
         self.toolbar_layout = QHBoxLayout(self)
@@ -96,44 +98,66 @@ class Page(QWidget):
             raise ValueError("Page's useragent is invalid (not 'pc' or 'mobile')")
         self.browser.page().profile().setHttpUserAgent(self.r_useragent)
         self.browser.stop()
-        if self.useragent == 'pc':
-            self.browser.resize(self.browser.parent().frameGeometry().width(),
-                                self.browser.parent().frameGeometry().height())
-            self.browser.setMaximumSize(self.browser.parent().frameGeometry().width(),
-                                        self.browser.parent().frameGeometry().height())
+        if self.useragent == "pc":
+            self.browser.resize(
+                self.browser.parent().frameGeometry().width(),
+                self.browser.parent().frameGeometry().height(),
+            )
+            self.browser.setMaximumSize(
+                self.browser.parent().frameGeometry().width(),
+                self.browser.parent().frameGeometry().height(),
+            )
             self.browser.move(0, 0)
-        elif self.useragent == 'mobile':
-            self.browser.resize(int(self.browser.parent().frameGeometry().height() * 9 / 16),
-                                self.browser.parent().frameGeometry().height())
-            self.browser.setMaximumSize(int(self.browser.parent().frameGeometry().height() * 9 / 16),
-                                        self.browser.parent().frameGeometry().height())
+        elif self.useragent == "mobile":
+            self.browser.resize(
+                int(self.browser.parent().frameGeometry().height() * 9 / 16),
+                self.browser.parent().frameGeometry().height(),
+            )
+            self.browser.setMaximumSize(
+                int(self.browser.parent().frameGeometry().height() * 9 / 16),
+                self.browser.parent().frameGeometry().height(),
+            )
             self.browser.move(
-                int(self.browser.parent().frameGeometry().width() / 2 - self.browser.frameGeometry().width() / 2), 0)
+                int(
+                    self.browser.parent().frameGeometry().width() / 2
+                    - self.browser.frameGeometry().width() / 2
+                ),
+                0,
+            )
         self.browser.reload()
-        self.toolbar['useragent'].setText('PC' if self.useragent == 'pc' else 'Mobile')
-        self.toolbar['useragent'].setIcon(
-            QIcon("assets/icons/useragent_pc.svg" if self.useragent == 'pc' else "assets/icons/useragent_mobile.svg"))
+        self.toolbar["useragent"].setText("PC" if self.useragent == "pc" else "Mobile")
+        self.toolbar["useragent"].setIcon(
+            QIcon(
+                "assets/icons/useragent_pc.svg"
+                if self.useragent == "pc"
+                else "assets/icons/useragent_mobile.svg"
+            )
+        )
 
     def __updatedURL(self, url):
-        self.toolbar['urlbar'].setText(url.toString())
+        self.toolbar["urlbar"].setText(url.toString())
 
     def __titleChanged(self, title):
         if self.window.pagebar.currentIndex() == self.pageselector:
             self.window.setWindowTitle(f"{BROWSER_NAME} - {title}")
-        title = title if len(title) < TAB_TITLE_CUTOFF else title[:TAB_TITLE_CUTOFF - 3] + "..."
+        title = (
+            title
+            if len(title) < TAB_TITLE_CUTOFF
+            else title[: TAB_TITLE_CUTOFF - 3] + "..."
+        )
         self.window.pagebar.setTabText(self.pageselector, title)
 
     def __iconChanged(self, icon):
         self.window.pagebar.setTabIcon(self.pageselector, icon)
 
     def __loadStarted(self):
-        self.toolbar['progressbar'].setValue(0)
+        self.toolbar["progressbar"].setValue(0)
         # if self.window.pagebar.currentIndex() == self.pageselector:
         #    self.window.setWindowTitle(f"{BROWSER_NAME} - Page #{self.pageselector + 1}")
         # self.window.pagebar.setTabText(self.pageselector, f"Page #{self.pageselector + 1}")
 
     def __loadFinished(self):
-        self.toolbar['progressbar'].setValue(100)
+        self.toolbar["progressbar"].setValue(100)
 
     def __loadProgress(self, n):
-        self.toolbar['progressbar'].setValue(n)
+        self.toolbar["progressbar"].setValue(n)
